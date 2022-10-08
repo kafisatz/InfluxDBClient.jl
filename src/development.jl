cfdd0724cc61a821fadc452b3b9fc41b0f500200
Bernhard König <github@diro.ch>
2022-10-08 21:22:13 +0200

    edt

--- src/development.jl
+++ src/development.jl
@@ -1,23 +1,36 @@
 using CodecZlib
 using TranscodingStreams
 
-strings = ["foo", "bar", "baz","asdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfasdfasdfasd8949318asdfa8s9df81asdf","asdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfasdfasdfasd8949318asdfa8s9df81asdf","asdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfasdfasdfasd8949318asdfa8s9df81asdf"]
+x=rand()
+strings = ["foo", "$x", x,"asdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfasdfasdfasd8949318asdfa8s9df81asdf","asdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfasdfasdfasd8949318asdfa8s9df81asdf","asdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfdasfasdfasdfasdfasdfasdfasd8949318asdfa8s9df81asdf"]
 length.(strings)
 codec = CodecZlib.GzipCompressor()
 TranscodingStreams.initialize(codec)  # allocate resources
-try
-    for s in strings
-        data = transcode(codec, s)
-        @show length(data)
-        # do something...
-    end
-catch
-    rethrow()
-finally
-    TranscodingStreams.finalize(codec)  # free resources
+data = UInt8[]
+for s in strings
+    d = transcode(codec, s)
+    #@show length(d)
+    append!(data,d)
+    # do something...
 end
 
+TranscodingStreams.finalize(codec)  # free resources
 
+data
+
+decompressed = transcode(GzipDecompressor,data)
+String(decompressed)
+
+#=
 using CodecZlib
 decompressed = transcode(ZlibDecompressor, b"x\x9cKL*JLNLI\x04R\x00\x19\xf2\x04U")
 String(decompressed)
+=#
+
+x
+io = IOBuffer();
+
+ write(io, "JuliaLang is a GitHub organization.", " It has many members.")
+write(io,x)
+
+ String(take!(io))
---
+++ src/development_str.jl
@@ -0,0 +1,40 @@
+using DataFrames 
+using CSV 
+
+xxx=DataFrame(x=rand(9),y=rand(9))
+
+@edit CSV.write(raw"C:\temp\mo.csv",xxx)
+
+Parsers.writeshortest
+
+#return Parsers.writeshortest(buf, pos, x, false, false, true, -1, UInt8('e'), false, opts.decimal)
+
+
+len = 2^22
+buf = Vector{UInt8}(undef, len)
+x = 15.1531536156581681616816881612122111232
+using Parsers
+using BenchmarkTools
+
+#may want to ensure that exp_form is false!
+    exp_form = true
+    pt = nexp + olength
+    if -4 < pt <= (precision == -1 ? (T == Float16 ? 3 : 6) : precision)
+        exp_form = false
+
+
+function test0(x) 
+    nd = Parsers.writeshortest(buf, 1, x, false, false, true, -1, UInt8('e'), false,'.')
+    return String(buf[1:nd])
+end
+function test1(x) 
+    return string(x)
+end
+
+test0(x)
+@btime test0(x)
+@btime test1(x)
+buf
+
+
+
