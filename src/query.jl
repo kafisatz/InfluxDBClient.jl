2e94d3a972f0ec6b482e1e273e5d523f9a0de9ea
Bernhard König <github@diro.ch>
2022-10-10 15:25:29 +0200

    fix host

--- src/query.jl
+++ src/query.jl
@@ -1,23 +1,23 @@
 #todo 
 @info("query functions are in the works...")
 
-function tmp(isettings,bucket,measurement;fields=Dict{String,Any}(),tags=Dict{String,Any}())
+function query_flux(isettings,bucket,measurement;range=Dict{String,Any}(),fields=Dict{String,Any}(),tags=Dict{String,Any}())
     #=
         df = DataFrame(_sensor_id = ["TLM0900","TLM0901","TLM0901"],other_tag=["m","m","x"] ,temperature = [73.1,55,22.0], humidity=[14.9,55.2,3], datetime = [some_dt,some_dt-Second(51),some_dt-Second(500)])
         lp = lineprotocol("my_meas",df,["temperature","humidity"], :datetime,compress = gzip_compression_is_enabled)
 
-        bucket = "a_random_bucket_name"
+        bucket = a_random_bucket_name
         measurement = "my_meas"
     =#
 
     @unpack INFLUXDB_HOST,INFLUXDB_TOKEN,INFLUXDB_ORG = isettings
 
-    q = """from(bucket: a_random_bucket_name) |> range(start: -100d) """
+    q = """from(bucket: "$(bucket)") |> range(start: -100d) """
 
-    hdrs = Dict("Authorization" => "Token $(INFLUXDB_TOKEN)", "Accept"=>"application/json","Content-Type"=>"application/json; charset=utf-8")
+    hdrs = Dict("Authorization" => "Token $(INFLUXDB_TOKEN)", "Accept"=>"application/json","Content-Type"=>"application/vnd.flux; charset=utf-8")
     hdrs["Content-Encoding"] = "identity"
     
-    url = """http://$(INFLUXDB_HOST)/api/v2/query?org=$INFLUXDB_ORG&bucket=$bucket&precision=$influx_precision"""
+    url = """http://$(INFLUXDB_HOST)/api/v2/query?org=$INFLUXDB_ORG"""
     bdy = q
 
     r = HTTP.request("POST", url, hdrs, body = bdy)
