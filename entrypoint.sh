9ee4e7d67f3a545921f881fd1c53b5d4b97a10f6
Bernhard König <github@diro.ch>
2022-10-10 11:52:50 +0200

    improve ci

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo "Running entrypoint.sh..."
+
+docker run -d -p 8086:8086 \
+    -e DOCKER_INFLUXDB_INIT_MODE=setup \
+    -e DOCKER_INFLUXDB_INIT_USERNAME="${INFLUXDB_USER}" \
+    -e DOCKER_INFLUXDB_INIT_PASSWORD="${INFLUXDB_PASSWORD}" \
+    -e DOCKER_INFLUXDB_INIT_ORG="${INFLUXDB_ORG}" \
+    -e DOCKER_INFLUXDB_INIT_ADMIN_TOKEN="${INFLUXDB_TOKEN}" \
+    -e DOCKER_INFLUXDB_INIT_BUCKET=my_bucket_unused influxdb:2.0
+      #-v $PWD/data:/var/lib/influxdb2 \
+      #-v $PWD/config:/etc/influxdb2 \
+      
